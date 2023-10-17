f7d99b6d7c8982ee2d3ec3dcb94adb9ba00a91a1
benn.diagram <benn.diagram@kakaocorp.com>
2023-10-17 16:53:21 +0900

    add gitignore

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,15 +1,11 @@
-from flask import Flask, request
-
+from flask import Flask
 app = Flask(__name__)
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/', methods=["POST", "GET"])
-    def index():
-        if (request.method == "POST"):
-            resp = request.get_json()
-            print(resp)
-
-    return "Done"
+    @app.route('/')
+    def hello_pybo():
+        return 'Hello, Pybo!'
 
     return app
